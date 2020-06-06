f38a19057946a70ece67ec1448e438f10ec802a3
horowolf <lucky801991@gmail.com>
2020-06-06 22:07:38 +0900

    Add skeleton class struct for index page, also linked with storyboard.

--- init.engineer-iOS-App/Controller/IndexTabController.swift
+++ init.engineer-iOS-App/Controller/IndexTabController.swift
@@ -11,11 +11,24 @@ import UIKit
 
 class IndexTabController: UIViewController {
 
+    @IBOutlet weak var turnipCalculatorImg: UIImageView!
+    @IBOutlet weak var facebookImg: UIImageView!
+    @IBOutlet weak var twitterImg: UIImageView!
+    @IBOutlet weak var plurkImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func turnipBtn(_ sender: Any) {
+    }
+    @IBAction func facebookBtn(_ sender: Any) {
+    }
+    @IBAction func twitterBtn(_ sender: Any) {
+    }
+    @IBAction func plurkBtn(_ sender: Any) {
+    }
+    
 }
 
