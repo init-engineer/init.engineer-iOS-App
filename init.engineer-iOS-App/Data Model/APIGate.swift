d1c37b4a4570e2040c9b450f389ec0514ba1d2ce
horowolf <lucky801991@gmail.com>
2020-06-09 02:55:03 +0900

    Start to make API connector.

---
+++ init.engineer-iOS-App/Data Model/APIGate.swift
@@ -0,0 +1,30 @@
+//
+//  APIGate.swift
+//  init.engineer-iOS-App
+//
+//  Created by horo on 6/9/20.
+//  Copyright © 2020 Kantai Developer. All rights reserved.
+//
+
+import Foundation
+
+class APIGate {
+    
+    func getArticleList(from page: Int) {
+        
+    }
+    
+    func getArticleDetails(from id: Int) {
+        
+    }
+    
+    func getArticleLinks(from id: Int) {
+        
+    }
+    
+    func getArticleComments(from id: Int, and page: Int) {
+        
+    }
+    
+    
+}
