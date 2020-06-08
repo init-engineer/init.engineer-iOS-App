be6be3913ea049dc9b11ccb63bd6bf11feacf65a
horowolf <lucky801991@gmail.com>
2020-06-09 02:53:41 +0900

    Linking storyboard contents with controllers

--- init.engineer-iOS-App/Controller/DashboardTabController.swift
+++ init.engineer-iOS-App/Controller/DashboardTabController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class DashboardTabController: UIViewController {
-
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
--- init.engineer-iOS-App/Controller/PublishTabController.swift
+++ init.engineer-iOS-App/Controller/PublishTabController.swift
@@ -9,11 +9,18 @@
 import UIKit
 
 class PublishTabController: UIViewController {
-
+    @IBOutlet weak var userImg: UIImageView!
+    @IBOutlet weak var articleImg: UIImageView!
+    @IBOutlet weak var articleText: UITextView!
+    @IBOutlet weak var themePicker: UIPickerView!
+    @IBOutlet weak var fontPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func imageUploadBtn(_ sender: Any) {
+    }
+    
 }
