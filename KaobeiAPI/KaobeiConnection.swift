9e7bafffe25ee7653f9bcb1577e0ec50ae4c2116
horowolf <lucky801991@gmail.com>
2020-10-12 02:17:03 +0900

    init api framework

---
+++ KaobeiAPI/KaobeiConnection.swift
@@ -0,0 +1,13 @@
+//
+//  KaobeiConnection.swift
+//  KaobeiAPI
+//
+//  Created by horo on 10/12/20.
+//  Copyright © 2020 Kantai Developer. All rights reserved.
+//
+
+import Foundation
+
+class KaobeiConnection {
+    
+}
