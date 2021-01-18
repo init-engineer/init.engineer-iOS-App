10b57a7939fc12e0c8f93a3a0f0783720347152c
horo <13122358+horowolf@users.noreply.github.com>
2021-01-18 16:24:19 +0900

    Moving keychain service name.

--- init.engineer-iOS-App/Token/KeyChainManager.swift
+++ init.engineer-iOS-App/Token/KeyChainManager.swift
@@ -12,9 +12,10 @@ import KeychainAccess
 class KeyChainManager {
     static let shared = KeyChainManager()
     private var token: String?
+    private let serviceName = "kaobei" // This name should chance to a constant value in a secret constant file.
     
     func saveToken(_ token: String) {
-        let key = Keychain(service: "kaobei")
+        let key = Keychain(service: serviceName)
         self.token = token
         key["token"] = token
     }
@@ -22,7 +23,7 @@ class KeyChainManager {
     func getToken() -> String {
         if self.token == nil {
             var token = ""
-            let key = Keychain(service: "kaobei")
+            let key = Keychain(service: serviceName)
             token = key["token"] ?? ""
             self.token = token
         }
