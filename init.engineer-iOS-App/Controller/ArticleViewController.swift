c41826d5a5e43920ca03f0d3b0dc606d048ef61f
horowolf <>
2021-02-09 01:26:22 +0900

    Implement article detail view
    Need to work in details.

--- init.engineer-iOS-App/Controller/ArticleViewController.swift
+++ init.engineer-iOS-App/Controller/ArticleViewController.swift
@@ -7,14 +7,12 @@
 //
 
 import UIKit
+import KaobeiAPI
 import GoogleMobileAds
 
 class ArticleViewController: UIViewController {
     @IBOutlet weak var articleImg: UIImageView!
     @IBOutlet weak var articleText: UITextView!
-    @IBOutlet weak var publishTimeLabel: UILabel!
-    @IBOutlet weak var anonymousImg: UIImageView!
-    @IBOutlet weak var anonymousLabel: UILabel!
     @IBOutlet weak var mainFBLikeLabel: UILabel!
     @IBOutlet weak var alterFBLikeLabel: UILabel!
     @IBOutlet weak var plurkLikeLabel: UILabel!
@@ -23,13 +21,107 @@ class ArticleViewController: UIViewController {
     @IBOutlet weak var alterFBShareLabel: UILabel!
     @IBOutlet weak var plurkShareLabel: UILabel!
     @IBOutlet weak var twitterShareLabel: UILabel!
-    @IBOutlet weak var commentListView: UIStackView!
+    @IBOutlet weak var commentListTable: UITableView!
+    
+    var commentList = [Comment]()
     
     var articleID: Int?
+    var count = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.commentListTable.delegate = self
+        self.commentListTable.dataSource = self
+        self.commentListTable.allowsSelection = false
+        self.articleText.isEditable = false
+        self.articleText.isSelectable = false
+        
+        // self.articleImg.addGestureRecognizer()
+        guard let id = self.articleID else { return } //back to article list
+        
+        let detailRequest = KBGetArticleDetail(id: id)
+        
+        KaobeiConnection.sendRequest(api: detailRequest) { [weak self] (response) in
+            switch response.result {
+                case .success(let data):
+                    self?.articleText.text = data.data.content
+                    do {
+                        try self?.articleImg.image = UIImage(data: Data(contentsOf: URL(string: data.data.image)!))
+                    } catch is Error {
+                        
+                    }
+                    break
+                case .failure(_):
+                    break
+            }
+        }
+        
+        let statsRequest = KBGetArticleStats(id: id)
+        
+        KaobeiConnection.sendRequest(api: statsRequest) { [weak self] (response) in
+            switch response.result {
+                case .success(let data):
+                    for item in data.data {
+                        if item.type == "twitter" {
+                            self?.twitterLikeLabel.text = "♥︎\(item.like)"
+                            self?.twitterShareLabel.text = "⎋\(item.share)"
+                        } else if item.type == "plurk" {
+                            self?.plurkLikeLabel.text = "♥︎\(item.like)"
+                            self?.plurkShareLabel.text = "⎋\(item.share)"
+                        } else if item.type == "facebook" {
+                            if item.connections == "primary" {
+                                self?.mainFBLikeLabel.text = "♥︎\(item.like)"
+                                self?.mainFBShareLabel.text = "⎋\(item.share)"
+                            } else if item.connections == "secondary" {
+                                self?.alterFBLikeLabel.text = "♥︎\(item.like)"
+                                self?.alterFBShareLabel.text = "⎋\(item.share)"
+                            }
+                        }
+                    }
+                   break
+                case .failure(_):
+                   break
+            }
+        }
+        
+        let commentRequest = KBGetArticleComments(id: id)
+        
+        KaobeiConnection.sendRequest(api: commentRequest) { [weak self] (response) in
+            switch response.result {
+                case .success(let data):
+                    self?.commentList.append(contentsOf: data.data)
+                    self?.count += 1
+                    self?.commentListTable.reloadData()
+                    break
+                case .failure(_):
+                    break
+            }
+        }
+        
     }
     
+}
+
+extension ArticleViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.commentList.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.commentListTable.dequeueReusableCell(withIdentifier: "comment") as! UITableViewCell
+        cell.textLabel?.text = self.commentList[indexPath.row].content
+        cell.textLabel?.textColor = .white
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let h: CGFloat = CGFloat((self.commentList[indexPath.row].content.count / 10)) * 20.0
+        return h
+    }
 }
