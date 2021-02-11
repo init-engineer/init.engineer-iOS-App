6361a708246f50f30145d3a8b26eb60cc8176782
tico88612 <tico88612@yahoo.com.tw>
2021-02-11 18:29:37 +0800

    Add two social media in Index Page
    Telegram 跟 Tumblr 的按鈕

--- init.engineer-iOS-App/Controller/IndexTabController.swift
+++ init.engineer-iOS-App/Controller/IndexTabController.swift
@@ -16,9 +16,11 @@ class IndexTabController: UIViewController, SFSafariViewControllerDelegate, Inde
     let indexPage = [
         ["線上抽籤系統", "搖ㄌㄟ搖ㄌㄟ搖", #imageLiteral(resourceName: "img_fortune"), "https://kaobei.engineer/fortunes"],
         ["大頭菜計算器", "輕鬆致富首選", #imageLiteral(resourceName: "kohlrabi"), "https://kaobei.engineer/animal/kohlrabi"],
+        ["Telegram 頻道", "史上最純的頻道", #imageLiteral(resourceName: "img_telegram"), "https://t.me/init_engineer"],
         ["Facebook 粉絲頁", "Hen 純", #imageLiteral(resourceName: "img_flag1"), "https://www.facebook.com/init.kobeengineer"],
         ["Plurk 粉絲頁", "Very 純", #imageLiteral(resourceName: "img_plurk"), "https://www.plurk.com/kaobei_engineer"],
         ["Twitter 粉絲頁", "Super 純", #imageLiteral(resourceName: "img_twitter"), "https://twitter.com/kaobei_engineer"],
+        ["Tumblr 粉絲頁", "湯現在不熱了", #imageLiteral(resourceName: "img_tumblr"), "https://kaobei-engineer.tumblr.com/"],
         ["Mewe 粉絲頁", "weeeeeeee~~", #imageLiteral(resourceName: "img_mewe"), "https://mewe.com/p/init.kobeengineer"],
     ]
     
