587ce836ba3f6d8a3753d0b584b0de41fdfc6f62
horowolf <13122358+horowolf@users.noreply.github.com>
2021-02-15 05:19:39 +0900

    code review - 2
    remove unused code.
    fix warning message.

--- init.engineer-iOS-App/Controller/DashboardTabController.swift
+++ init.engineer-iOS-App/Controller/DashboardTabController.swift
@@ -188,31 +188,3 @@ extension DashboardTabController: ArticleCellDelegate {
         }
     }
 }
-
-
-extension DashboardTabController {
-    func addBannerViewToView(_ bannerView: GADBannerView) {
-        bannerView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(bannerView)
-        view.addConstraints(
-          [NSLayoutConstraint(item: bannerView,
-                              attribute: .bottom,
-                              relatedBy: .equal,
-                              toItem: bottomLayoutGuide,
-                              attribute: .top,
-                              multiplier: 1,
-                              constant: 0),
-           NSLayoutConstraint(item: bannerView,
-                              attribute: .centerX,
-                              relatedBy: .equal,
-                              toItem: view,
-                              attribute: .centerX,
-                              multiplier: 1,
-                              constant: 0)
-          ])
-       }
-    
-    func adViewDidReceiveAd(_ bannerView: GADBannerView) {
-        addBannerViewToView(bannerView)
-    }
-}
