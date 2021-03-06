f793fef2778f0970d1eb3354dd7c0126666e31bf
stephen <tasb00429@gmail.com>
2021-03-07 00:16:18 +0800

    [ Update ]：uncomment pod install

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -3,7 +3,7 @@
 cd ../BuildConfigure || exit
 
 echo "Pod install"
-# pod install
+pod install
 
 echo "Copy and rename files, Ex Debug.xcconfig.example to Debug.xcconfig"
 find . -name "*.example" -exec sh -c 'cp {} `echo {} | sed 's/\.example//g'`' \;
