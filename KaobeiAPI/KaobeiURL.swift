e09b5210d42aadde1f8668c1b94400e887dfb0c1
tico88612 <17496418+tico88612@users.noreply.github.com>
2021-03-21 23:10:38 +0800

    Change Base URL

--- KaobeiAPI/KaobeiURL.swift
+++ KaobeiAPI/KaobeiURL.swift
@@ -9,29 +9,29 @@
 import Foundation
 
 struct KaobeiURL {
-    static let basePath = "https://kaobei.engineer/"
+    static let basePath = "https://init.engineer/"
     //取得文章列表 (Int)
-    static let articleList = "https://kaobei.engineer/api/frontend/social/cards?page=%d"
+    static let articleList = "https://init.engineer/api/frontend/social/cards?page=%d"
     //取得文章詳細資訊 (Int)
-    static let articleDetail = "https://kaobei.engineer/api/frontend/social/cards/%d/show"
+    static let articleDetail = "https://init.engineer/api/frontend/social/cards/%d/show"
     //取得文章社群連結、讚數、分享數(Int)
-    static let articleStats = "https://kaobei.engineer/api/frontend/social/cards/%d/links"
+    static let articleStats = "https://init.engineer/api/frontend/social/cards/%d/links"
     //取得文章回覆(Int, Int)
-    static let articleComments = "https://kaobei.engineer/api/frontend/social/cards/%d/comments?page=%d"
+    static let articleComments = "https://init.engineer/api/frontend/social/cards/%d/comments?page=%d"
     //（AUTH：API）取得使用者個人資訊
-    static let userProfile = "https://kaobei.engineer/api/frontend/user/profile"
+    static let userProfile = "https://init.engineer/api/frontend/user/profile"
     //（AUTH：API）取得自己文章列表(Int)
-    static let userPosts = "https://kaobei.engineer/api/frontend/social/cards/api/dashboard?page=%d"
+    static let userPosts = "https://init.engineer/api/frontend/social/cards/api/dashboard?page=%d"
     //（AUTH：API）發表文章
-    static let userPublishing = "https://kaobei.engineer/api/frontend/social/cards/api/publish"
+    static let userPublishing = "https://init.engineer/api/frontend/social/cards/api/publish"
     //OAuth 取得授權碼(Int, String, String, String)
-    static let requestGrant = "https://kaobei.engineer/oauth/authorize?client_id=%d&redirect_uri=%@&response_type=%@&scope=%@"
+    static let requestGrant = "https://init.engineer/oauth/authorize?client_id=%d&redirect_uri=%@&response_type=%@&scope=%@"
     //OAuth 取得 access_token
-    static let requestToken = "https://kaobei.engineer/oauth/token"
+    static let requestToken = "https://init.engineer/oauth/token"
     //（AUTH : API）取得需要審核的文章列表
-    static let articleReviewList = "https://kaobei.engineer/api/frontend/social/cards/api/review?page=%d"
+    static let articleReviewList = "https://init.engineer/api/frontend/social/cards/api/review?page=%d"
     //（AUTH : API）將文章審核通過(Int)
-    static let articleVoteAye = "https://kaobei.engineer/api/frontend/social/cards/api/review/%d/succeeded"
+    static let articleVoteAye = "https://init.engineer/api/frontend/social/cards/api/review/%d/succeeded"
     //（AUTH : API）將文章審核不通過(Int)
-    static let articleVoteNay = "https://kaobei.engineer/api/frontend/social/cards/api/review/%id/failed"
+    static let articleVoteNay = "https://init.engineer/api/frontend/social/cards/api/review/%id/failed"
 }
